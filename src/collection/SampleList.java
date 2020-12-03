303cf40069361b21143d87d6f623b61044383f78
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-03 23:54:18 +0530

    create SampleList.java

---
+++ src/collection/SampleList.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 03-Dec-20
+ *   Time: 11:53 PM
+ *   File: SampleList.java
+ */
+
+package collection;
+
+public class SampleList {
+}
+
