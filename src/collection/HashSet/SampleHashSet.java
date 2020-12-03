935b7bffbd23cad7a24d850866cb3c6132e3ecf5
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-04 00:29:32 +0530

    implement methods of Hashset

--- src/collection/HashSet/SampleHashSet.java
+++ src/collection/HashSet/SampleHashSet.java
@@ -7,6 +7,27 @@
 
 package collection.HashSet;
 
+import java.util.HashSet;
+
 public class SampleHashSet {
+    public static void main(String[] args) {
+        HashSet<Integer> rollNumber= new HashSet<>();
+        rollNumber.add(23);
+        rollNumber.add(2);
+        rollNumber.add(3);
+        rollNumber.add(4);
+        rollNumber.add(5);
+        rollNumber.add(10);
+        rollNumber.add(7);
+        rollNumber.add(21);
+        rollNumber.add(1);
+        System.out.println(rollNumber);
+        rollNumber.add(1);
+        System.out.println(rollNumber);
+        System.out.println("Size of hashset(rollNumber) :- "+rollNumber.size());
+        //rollNumber.clear();
+        System.out.println(rollNumber);
+
+    }
 }
 
