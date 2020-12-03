880379ea1a1f8dc41733973fae4acd24e41e5f91
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-04 00:10:15 +0530

    implement some method with using of class of list

--- src/collection/SampleList.java
+++ src/collection/SampleList.java
@@ -7,7 +7,9 @@
 
 package collection;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class SampleList {
     public static void main(String[] args) {
@@ -19,7 +21,52 @@ public class SampleList {
         System.out.println(list);
         list.remove(1);
         System.out.println(list);
-        System.out.println("Size of list :- "+list.size());
+        System.out.println("Size of list :- " + list.size());
+
+
+        ArrayList<Contact> contactsList = new ArrayList<>();
+        Contact friend1 = new Contact("Sam", "Singh", 1234569875);
+        Contact friend2 = new Contact("Dam", "Singh", 1234889875);
+        contactsList.add(friend1);
+        contactsList.add(friend2);
+        System.out.println(contactsList);
+
+
     }
 }
 
+class Contact {
+    private String contactFirstName;
+    private String contactlastName;
+    private long phoneNumber;
+
+    public Contact(String contactFirstName, String contactlastName, long phoneNumber) {
+        this.contactFirstName = contactFirstName;
+        this.contactlastName = contactlastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "contactFirstName='" + contactFirstName + '\'' +
+                ", contactlastName='" + contactlastName + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return phoneNumber == contact.phoneNumber &&
+                Objects.equals(contactFirstName, contact.contactFirstName) &&
+                Objects.equals(contactlastName, contact.contactlastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contactFirstName, contactlastName, phoneNumber);
+    }
+}
