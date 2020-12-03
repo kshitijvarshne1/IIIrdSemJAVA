ae5fe6df5803b87cced21feab707f960088b8173
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-04 00:22:39 +0530

    create SampleHashSet.java

---
+++ src/collection/HashSet/SampleHashSet.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 04-Dec-20
+ *   Time: 12:22 AM
+ *   File: SampleHashSet.java
+ */
+
+package collection.HashSet;
+
+public class SampleHashSet {
+}
+
