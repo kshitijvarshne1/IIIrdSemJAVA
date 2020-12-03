964534f508ccb08e62ccc34aeee678962b5c01f6
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-04 00:00:25 +0530

    implement some method of list

--- src/collection/SampleList.java
+++ src/collection/SampleList.java
@@ -7,9 +7,19 @@
 
 package collection;
 
+import java.util.ArrayList;
+
 public class SampleList {
     public static void main(String[] args) {
-
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(5);
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        System.out.println("Size of list :- "+list.size());
     }
 }
 
