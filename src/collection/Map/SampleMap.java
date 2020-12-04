9da0afd7e54e3057078aa028a28d4a82a219bbd8
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-04 13:56:52 +0530

    implements some methods of HashMap

--- src/collection/Map/SampleMap.java
+++ src/collection/Map/SampleMap.java
@@ -7,6 +7,17 @@
 
 package collection.Map;
 
+import java.util.HashMap;
+
 public class SampleMap {
+    public static void main(String[] args) {
+        HashMap<Integer, String> rollNumbers = new HashMap<>();
+        rollNumbers.put(1, "Sam");
+        rollNumbers.put(2, "Kam");
+        rollNumbers.put(3, "Tam");
+        System.out.println(rollNumbers);
+        System.out.println("name of rollnumber 2 :- " + rollNumbers.get(2));
+
+    }
 }
 
