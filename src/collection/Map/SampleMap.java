d096715756c6100266b5bdd151ea8351fd5747c1
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-04 13:46:56 +0530

    create SampleMap.java

---
+++ src/collection/Map/SampleMap.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 04-Dec-20
+ *   Time: 1:46 PM
+ *   File: SampleMap.java
+ */
+
+package collection.Map;
+
+public class SampleMap {
+}
+
