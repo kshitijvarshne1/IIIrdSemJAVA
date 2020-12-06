66bb0f35d6881435f6417ce871c085ba6e99b7c7
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-06 13:38:10 +0530

    define the class Student.java

--- src/StreamAPI/student/Student.java
+++ src/StreamAPI/student/Student.java
@@ -7,6 +7,66 @@
 
 package StreamAPI.student;
 
+import java.util.Objects;
+
 public class Student {
+    private int rollNumber;
+    private String name;
+    private double cpi;
+
+    public Student(int rollNumber, String name, double cpi) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.cpi = cpi;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCpi() {
+        return cpi;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCpi(double cpi) {
+        this.cpi = cpi;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return getRollNumber() == student.getRollNumber() &&
+                Double.compare(student.getCpi(), getCpi()) == 0 &&
+                Objects.equals(getName(), student.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRollNumber(), getName(), getCpi());
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "%d. %s: %.2f",
+                this.getRollNumber(),
+                this.getName(),
+                this.getCpi()
+        );
+    }
 }
 
