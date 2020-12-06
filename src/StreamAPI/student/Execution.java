2000c50780849579a8fc912401dbd39274714cb6
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-06 14:02:10 +0530

    final commit

--- src/StreamAPI/student/Execution.java
+++ src/StreamAPI/student/Execution.java
@@ -7,6 +7,24 @@
 
 package StreamAPI.student;
 
+import java.util.ArrayList;
+
 public class Execution {
+    private static final int CLASS_SIZE = 10;
+
+    public static void main(String[] args) {
+        ArrayList<Student> studentList = new ArrayList<>();
+        for (int i = 0; i < CLASS_SIZE; i++) {
+            studentList.add(
+                    new Student(
+                            (i + 1),
+                             "Name " + (i + 1),
+                            Math.random() * 10.0
+                    )
+            );
+        }
+        var studentStream = studentList.stream();
+        studentStream.filter(student -> student.getCpi() > 8.5).forEach(System.out::println);
+    }
 }
 
