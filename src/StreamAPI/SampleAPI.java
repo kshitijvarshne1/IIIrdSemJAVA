f33fa063ea19cd3196d4895a559f3be8c812563c
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-06 10:32:02 +0530

    create SampleAPI.java

---
+++ src/StreamAPI/SampleAPI.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 06-Dec-20
+ *   Time: 10:31 AM
+ *   File: SampleAPI.java
+ */
+
+package StreamAPI;
+
+public class SampleAPI {
+}
+
