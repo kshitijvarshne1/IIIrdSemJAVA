d166cbbd1188a9d36c60a1eefade949323c40c32
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-06 10:58:53 +0530

    final commit SampleAPI.java

--- src/StreamAPI/SampleAPI.java
+++ src/StreamAPI/SampleAPI.java
@@ -7,6 +7,39 @@
 
 package StreamAPI;
 
+import java.util.ArrayList;
+
 public class SampleAPI {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(12);
+        list.add(15);
+        list.add(18);
+        list.add(21);
+        list.add(23);
+        System.out.println("Original list :- " + list);
+        //Imperitive approach
+        //approach use to find out smallest number
+        int smallestNumber = list.get(0);
+        for (Integer number : list) {
+            if (number < smallestNumber) {
+                smallestNumber = number;
+            }
+        }
+        System.out.println("smallest number :- " + smallestNumber);
+
+        //Declarative approach
+        //use to find smallest number in list
+
+        //1.Create a stream
+        var stream = list.stream();
+        //2.Intermediate operation
+        //Declarative style of programming(defining what to do)
+        var smallestNumber2 = stream.min(Integer::compare);
+        System.out.println("Smallest number :- " + smallestNumber2);
+
+
+    }
 }
 
