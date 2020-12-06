3f8272b18dd52ee50e9418b4d2cb9bb505b7209e
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-06 13:33:05 +0530

    create Student.java

---
+++ src/StreamAPI/student/Student.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 06-Dec-20
+ *   Time: 1:32 PM
+ *   File: Student.java
+ */
+
+package StreamAPI.student;
+
+public class Student {
+}
+
