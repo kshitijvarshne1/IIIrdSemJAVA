7c2e36889eaea0d66a1f8fc25024f0a255c3d6e4
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-08 21:24:42 +0530

    final

--- src/Regex/SampleRegex.java
+++ src/Regex/SampleRegex.java
@@ -13,11 +13,21 @@ import java.util.regex.Pattern;
 public class SampleRegex {
     public static void main(String[] args) {
         //Step1-> Use the pattern class to define a regular expression
-        Pattern pattern = Pattern.compile("^\\d{4}[ ]\\d{4}[ ]\\d{4}[ ]$");  //Regular Expression
+        Pattern pattern = Pattern.compile("^\\d{4}[ ]\\d{4}[ ]\\d{4}$");  //Regular Expression
         //Step 2:- Match the pattern a sequence of characters using the Matcher class
-        Matcher matcher = pattern.matcher("1234 5678 1234");  //Test String
+        Matcher matcher = pattern.matcher("1234 5678 1289");  //Test String
         // Methods from the java.Util.regex.Matcher Class
-
+        /*
+        * 1. matched() method
+        * It is the simplest pattern matching method.It determines whether
+        * the character sequence (test string) matches the pattern (regex).
+        */
+        if(matcher.matches()){
+            System.out.println("The test string is passed");
+        }
+        else{
+            System.out.println("The test string is failed ");
+        }
     }
 }
 
