aeb681de520ee8a75958ea056efcf117558345f8
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-08 21:17:30 +0530

    implement Matcher and Pattern class

--- src/Regex/SampleRegex.java
+++ src/Regex/SampleRegex.java
@@ -7,6 +7,17 @@
 
 package Regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SampleRegex {
+    public static void main(String[] args) {
+        //Step1-> Use the pattern class to define a regular expression
+        Pattern pattern = Pattern.compile("^\\d{4}[ ]\\d{4}[ ]\\d{4}[ ]$");  //Regular Expression
+        //Step 2:- Match the pattern a sequence of characters using the Matcher class
+        Matcher matcher = pattern.matcher("1234 5678 1234");  //Test String
+        // Methods from the java.Util.regex.Matcher Class
+
+    }
 }
 
