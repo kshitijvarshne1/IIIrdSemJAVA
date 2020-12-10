81d08ad1df6840ca6921732f6424b3f6936a3536
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-10 12:31:03 +0530

    create Sample2.java

---
+++ src/GenericProgramming/Sample2.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 10-Dec-20
+ *   Time: 12:30 PM
+ *   File: Sample2.java
+ */
+
+package GenericProgramming;
+
+public class Sample2 {
+}
+
