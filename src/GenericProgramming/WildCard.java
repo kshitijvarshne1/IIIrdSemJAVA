295e5bed2fccf2d9b96b9e6ed769423731c9a41c
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-10 13:55:46 +0530

    create WildCard.java

---
+++ src/GenericProgramming/WildCard.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 10-Dec-20
+ *   Time: 1:55 PM
+ *   File: WildCard.java
+ */
+
+package GenericProgramming;
+
+public class WildCard {
+}
+
